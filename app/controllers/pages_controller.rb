d0f6c2c1653863343622e1725aabe6916905da16
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2014-09-30 20:46:28 +0900

    customize bootstrap variables
    This is where you created a heroku URL and planned to push your app to
    Heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
 
   def about
   end
+
+
+  def links
+  end
+
+
+  def worksheets
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get 'pages/home'
   root "pages#home"
   get "about" => "pages#about"
+  get "links" => "pages#links"
+  get "worksheets" => "pages#worksheets"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
